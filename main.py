d8882872c5d28fd86e4ba5c119bb64432009df87
Emlyn O'Regan <emlynoregan@gmail.com>
2015-07-18 12:46:31 +0000

    changed message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Yo Dawg!')
+        self.response.write('Yo Dawg, I hear you like old memes.')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
